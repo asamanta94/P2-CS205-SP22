680bf5d351e40890e6fba855c3ba1c765de3a156
Aritra Samanta <asama004@ucr.edu>
2022-05-30 17:36:54 -0700

    Finished forward selection.
    - Added comments and print statements.
    - Tested with small dataset.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 from os import path
 
 from knn import KNN
-from selection import Selection, FORWARD_SELECTION, BACKWARD_SELECTION
+from selection import Selection
 
 DATASETS_DIR = "dataset"
 DATA_SMALL_FILE = "CS205_SP_2022_SMALLtestdata__64.txt"
@@ -14,14 +14,28 @@ LARGE_DATASET_PATH = path.join(DATASETS_DIR, DATA_LARGE_FILE)
 
 
 if __name__ == '__main__':
+    print("Welcome to Aritra Samanta's Feature Selection Algorithm.")
+    file_path = input("Type in the name of the file to test: ")
+
+    algorithm = int(input("Type the number of the algorithm you want to run. "
+                      "(1 - Forward Selection, 2 - Backward Elimination): "))
+
     # Get the dataset from file
-    dataset = np.genfromtxt(SMALL_DATASET_PATH)
+    dataset = np.genfromtxt(file_path)
+    n_features = len(dataset[0][1:])
+    n_instances = len(dataset)
+
+    print("\nThis dataset has {} features (not including the class attribute), with {} instances.\n"
+          .format(n_features, n_instances))
 
     # Run the dataset against the classifier for predicting accuracy
     classifier = KNN(1, dataset)
-    classifier.feature_list = (1, 2)
     accuracy = classifier.evaluate()
-    print(accuracy)
 
-    s = Selection(FORWARD_SELECTION, dataset)
+    print("Running nearest neighbor with all {0} features, using \"leaving-one-out\" evaluation, I get"
+          " an accuracy of {1:.2f}%.\n".format(n_features, accuracy * 100))
+
+    # Run feature selection algorithm
+    print("Beginning Search.\n")
+    s = Selection(algorithm, dataset, classifier)
     s.search()
--- selection.py
+++ selection.py
@@ -1,20 +1,31 @@
-FORWARD_SELECTION = 0
-BACKWARD_SELECTION = 1
+FORWARD_SELECTION = 1
+BACKWARD_ELIMINATION = 2
 
 
 class Selection(object):
 
-    def __init__(self, way, data_set):
+    def __init__(self, way, data_set, classifier):
         self.way = way
         self.data_set = data_set
         self.n_features = len(self.data_set[0][1:])
+        self.classifier = classifier
         self.visited = set()
 
     def get_features(self, features):
+        """
+        Get the next set of features for the given set of features.
+
+        :param features: The current set of features.
+        :return: The next set of possible features.
+        """
         children = []
+
         for i in range(self.n_features):
+            # If feature is already in the feature set no need to add.
             if i in features:
                 continue
+
+            # Create a new feature and add it to the set.
             child = set(features)
             child.add(i)
             child = tuple(child)
@@ -28,18 +39,51 @@ class Selection(object):
         accuracies = []
         features = []
 
+        best_accuracy = -1.0
+        best_features = ()
+
         if self.way == FORWARD_SELECTION:
             features.append(())
             self.visited.add(())
-            while len(features) > 0:
+
+            while True:
+                # Get the best feature
                 top = features[0]
-                print(top)
                 features.remove(top)
 
+                # Expand the possible set of features.
                 children = self.get_features(top)
                 self.visited.add(top)
-                # Get the greediest child
 
+                # If no more features can be added, then search is done.
+                if len(children) == 0:
+                    break
+
+                # Reset local parameters
+                current_best_accuracy = -1.0
+                current_best_features = ()
+
+                # For each child feature, pick the feature set with the best accuracy and add it to
+                # the features list.
                 for child in children:
                     if child not in features:
-                        features.append(child)
+                        self.classifier.feature_list = child
+                        accuracy = self.classifier.evaluate()
+                        if accuracy > current_best_accuracy:
+                            current_best_accuracy = accuracy
+                            current_best_features = child
+
+                        print("\tUsing feature(s) {0} accuracy is {1:.2f}%.".format(child, accuracy * 100))
+
+                print("\nFeature set {0} was best, accuracy is {1:.2f}%.\n".format(current_best_features,
+                                                                                   current_best_accuracy * 100))
+                features.append(current_best_features)
+
+                if best_accuracy < current_best_accuracy:
+                    best_accuracy = current_best_accuracy
+                    best_features = current_best_features
+
+                # TODO: ACCURACY DESCREASED CALCULATION AND PRINT
+
+            print("\nFinished search!! The best feature subset is {0} which has an accuracy of {1:.2f}%.\n"
+                  .format(best_features, best_accuracy * 100))
